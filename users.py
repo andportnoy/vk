04755b0d4e96349b10f81f0e025defb0db11d22f
Andrey Portnoy <aportnoy@ucsd.edu>
2016-06-05 01:55:55 -0700

    add type checking in users.get

--- users.py
+++ users.py
@@ -1,18 +1,31 @@
 from . import core
 
 
-def get(user_ids=None, fields=None, name_case=None):
+def get(access_token=None, user_ids=None, fields=None, name_case=None):
     """user_ids and fields have to be lists."""
 
-    if not isinstance(user_ids, list):
-        raise TypeError('user_ids must be a list.')
+    # check type, joining is done later batch by batch
+    if user_ids is not None:
+        if not isinstance(user_ids, list):
+            raise TypeError('user_ids must be a list.')
 
+    # check type and join
     if fields is not None:
-        fields = ','.join(fields)
+        if isinstance(fields, list):
+            fields = ','.join(fields)
+        else:
+            raise TypeError('fields must be a list.')
 
+    # check type
+    if name_case is not None:
+        if not isinstance(name_case, str):
+            raise TypeError('name_case must be a string.')
+
+    # create params dictionary from passed arguments,
+    # but put user_ids in a separate dict
     params = core.params_dict_from_locals(locals())
     user_ids = {'user_ids': params.pop('user_ids')}
-    
+
     # the API method only accepts at most 1000 user ids,
     # but in reality it can't handle more than 900 long ids,
     # possibly because of commas overhead
@@ -26,7 +39,8 @@ def get(user_ids=None, fields=None, name_case=None):
     return result
 
 
-def _get_batch_of_users(user_ids=None, fields=None, name_case=None):
+def _get_batch_of_users(access_token=None, user_ids=None,
+                        fields=None, name_case=None):
 
     user_ids = ','.join(str(uid) for uid in user_ids)
 
