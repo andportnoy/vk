37011e2efa142fbb07b4d84a5ae7dd5e6da6b250
Andrey Portnoy <aportnoy@ucsd.edu>
2016-06-04 20:12:18 -0700

    users.get with batch abstraction and params construction from locals

--- users.py
+++ users.py
@@ -1,47 +1,36 @@
-import math
-
 from . import core
 
 
-def get(access_token=None, user_ids=None, fields=None, name_case=None):
+def get(user_ids=None, fields=None, name_case=None):
+    """user_ids and fields have to be lists."""
 
-    """user_ids has to be a list of ids"""
-    user_ids = ','.join(str(uid) for uid in user_ids)
-    fields = ','.join(fields)
+    if not isinstance(user_ids, list):
+        raise TypeError('user_ids must be a list.')
 
+    if fields is not None:
+        fields = ','.join(fields)
+
+    params = core.params_dict_from_locals(locals())
+    user_ids = {'user_ids': params.pop('user_ids')}
+    
     # the API method only accepts at most 1000 user ids,
-    # so if there is more, we organize them in batches
-
-    batch_size = 1000
-    n_ids = len(user_ids)
-
-    result = []
-    if n_ids > batch_size:
-        n_batches = int(math.ceil(n_ids/float(batch_size)))
-        print(n_batches, 'batches.')
-        for i in range(n_batches):
-            batch = user_ids[i: (i+1) * batch_size]
-            result += _get_batch(access_token=access_token, user_ids=batch,
-                                 fields=fields, name_case=name_case)
-            print('Batch', i+1, 'out of', n_batches, 'received.')
-    else:
-        return _get_batch(access_token=access_token, user_ids=user_ids,
-                          fields=fields, name_case=name_case)
+    # but in reality it can't handle more than 900 long ids,
+    # possibly because of commas overhead
+    # for explanation of the choice of batch_size, see relevant notebook
+    batch_size = 900
+    result = core.execute_in_batches(_get_batch_of_users,
+                                     user_ids,
+                                     batch_size,
+                                     params)
 
     return result
 
 
-def _get_batch(access_token=None, user_ids=None, fields=None, name_case=None):
-    params_dict = {}
-    if access_token:
-        params_dict['access_token'] = access_token
-    if user_ids:
-        params_dict['user_ids'] = user_ids
-    if fields:
-        params_dict['fields'] = fields
-    if name_case:
-        params_dict['name_case'] = name_case
+def _get_batch_of_users(user_ids=None, fields=None, name_case=None):
 
+    user_ids = ','.join(str(uid) for uid in user_ids)
+
+    params_dict = core.params_dict_from_locals(locals())
     result = core.vdr('users.get', params_dict=params_dict)
 
     return result
