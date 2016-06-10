74c5a7df8f7587d591ac03cea81f642ff5bee0e5
Andrey Portnoy <aportnoy@ucsd.edu>
2016-06-10 03:09:29 -0700

    refactor wall.getById using params_dict_from_locals

--- wall.py
+++ wall.py
@@ -26,23 +26,15 @@ def get(access_token=None, owner_id=None, domain=None, offset=None, count=None,
     return result
 
 
-def getById(posts=None, extended=None, copy_history_depth=None, fields=None):
 def get_count(access_token=None, owner_id=None, domain=None, filter=None):
     params_dict = core.params_dict_from_locals(locals())
     return _get_batch_of_posts(**params_dict, count=1)['count']
 
+
+def getById(posts=None, extended=None, copy_history_depth=None, fields=None):
+
     """posts is a comma-joined string of post ids."""
-    params_dict = {}
-
-    if posts:
-        params_dict['posts'] = posts
-    if extended:
-        params_dict['extended'] = extended
-    if copy_history_depth:
-        params_dict['copy_history_depth'] = copy_history_depth
-    if fields:
-        params_dict['fields'] = fields
-
+    params_dict = core.params_dict_from_locals(locals())
     result = core.vdr('wall.getById', params_dict)
 
     return result
