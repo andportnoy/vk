40e1bf8dc6be9e962820e7484f28746ef96d7259
Andrey Portnoy <aportnoy@ucsd.edu>
2016-06-09 23:11:45 -0700

    added places to __init__.py

--- __init__.py
+++ __init__.py
@@ -2,4 +2,7 @@ from .tokensession import TokenSession
 from . import users
 from . import friends
 from . import wall
+from . import places
 from .core import read_token
+# TODO add checking for Python version (Python 3 is mandatory)
+
